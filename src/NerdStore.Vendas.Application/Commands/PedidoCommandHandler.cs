b6b3da76694ba43de945b267c10b02593de9a437
Antonio Jose Sucre Gonzalez <antonio.gonzalez@efrete.com.br>
2023-01-15 16:51:41 -0300

    Command e CommandHandler

---
+++ src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using NerdStore.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class PedidoCommandHandler : IRequestHandler<AdicionarItemPedidoCommand, bool>
+    {
+        public async Task<bool> Handle(AdicionarItemPedidoCommand request, CancellationToken cancellationToken)
+        {
+            if (ValidarComando(request)) return false;
+
+            return true;
+        }
+
+        private bool ValidarComando(Command request)
+        {
+            if (request.EhValido()) return true;
+
+            foreach (var error in request.ValidationResult.Errors)
+            {
+                // lançar evento de erro
+            }
+
+            return false;
+        }
+    }
+}

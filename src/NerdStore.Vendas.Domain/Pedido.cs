79dc677a8b3fe232396726fac1aa4f0ddf1633b8
Antonio Jose Sucre Gonzalez <antonio.gonzalez@efrete.com.br>
2023-01-15 15:44:19 -0300

    Objetos de dominio Vendas

--- src/NerdStore.Vendas.Domain/Pedido.cs
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -9,6 +9,168 @@ namespace NerdStore.Vendas.Domain
 {
     public class Pedido : Entity, IAggregateRoot
     {
+        public int Codigo { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Guid? VoucherId { get; private set; }
+        public bool VoucherUtilizado { get; private set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        // EF Rel.
+        public virtual Voucher Voucher { get; private set; }
+
+        public Pedido(Guid clienteId, bool voucherUtilizado, decimal desconto, decimal valorTotal)
+        {
+            ClienteId = clienteId;
+            VoucherUtilizado = voucherUtilizado;
+            Desconto = desconto;
+            ValorTotal = valorTotal;
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        protected Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        public void CalcularValorPedido()
+        {
+            ValorTotal = PedidoItems.Sum(item => item.CalcularValor());
+            CalcularValorTotalDesconto();
+        }
+
+        public void AplicarVoucher(Voucher voucher)
+        {
+            Voucher = voucher;
+            VoucherUtilizado = true;
+            CalcularValorPedido();
+        }
+
+        public void CalcularValorTotalDesconto()
+        {
+            if (!VoucherUtilizado) return;
+
+            decimal desconto = 0;
+            var valor = ValorTotal;
+
+            if (Voucher.TipoDescontoVoucher == TipoDescontoVoucher.Porcentagem)
+            {
+                if (Voucher.Percentual.HasValue)
+                {
+                    desconto = (valor * Voucher.Percentual.Value) / 100;
+                    valor -= desconto;
+                }
+            }
+            else
+            {
+                if (Voucher.ValorDesconto.HasValue)
+                {
+                    desconto = Voucher.ValorDesconto.Value;
+                    valor -= desconto;
+                }
+            }
+
+            ValorTotal = valor < 0 ? 0 : valor;
+            Desconto = desconto;
+        }
+
+        public bool PedidoItemExistente(PedidoItem item)
+        {
+            return _pedidoItems.Any(i => i.ProdutoId == item.ProdutoId);   
+        }
+
+        public void AdicionarItem(PedidoItem item)
+        {
+            if (!item.EhValido()) return;
+
+            item.AssociarPedido(Id);
+
+            if (PedidoItemExistente(item))
+            {
+                var itemExistente = _pedidoItems.FirstOrDefault(i => i.ProdutoId == item.ProdutoId);
+                itemExistente.AtualizarUnidades(item.Quantidade);
+                item = itemExistente;
+
+                _pedidoItems.Remove(itemExistente);
+            }
+
+            item.CalcularValor();
+            _pedidoItems.Add(item);
+
+            CalcularValorPedido();
+        }
+
+        public void RemoverItem(PedidoItem item)
+        {
+            if (!item.EhValido()) return;
+
+            var itemExistente = PedidoItems.FirstOrDefault(i => i.ProdutoId == item.Id);
+
+            if (itemExistente == null) throw new DomainException("O item não pertence ao pedido");
+            _pedidoItems.Remove(item);
+
+            CalcularValorPedido();
+        }
+
+        public void AtualizarItem(PedidoItem item)
+        {
+            if (!item.EhValido()) return;
+            item.AssociarPedido(Id);
+
+            var itemExistente = PedidoItems.FirstOrDefault(i => i.PedidoId == item.Id);
+
+            if (itemExistente == null) throw new DomainException("O item não pertence ao pedido");
+
+            _pedidoItems.Remove(itemExistente);
+            _pedidoItems.Add(item);
+
+            CalcularValorPedido();
+        }
+
+        public void AtualizarUnidade(PedidoItem item, int unidades)
+        {
+            item.AtualizarUnidades(unidades);
+            AtualizarItem(item);
+        }
+
+        public void TornarRascunho()
+        {
+            PedidoStatus = PedidoStatus.Rascunho;
+        }
+
+        public void IniciarPedido()
+        {
+            PedidoStatus = PedidoStatus.Iniciado;
+        }
+
+        public void FinalizarPedido()
+        {
+            PedidoStatus = PedidoStatus.Pago;
+        }
+
+        public void CancelarPedido()
+        {
+            PedidoStatus = PedidoStatus.Cancelado;
+        }
+
+        public static class PedidoFactory
+        {
+            public static Pedido NovoPedidoRascunho(Guid clienteId)
+            {
+                var pedido= new Pedido()
+                {
+                    ClienteId = clienteId,
+                };
+
+                pedido.TornarRascunho();
+                return pedido;
+            }
+        }
 
     }
 }
